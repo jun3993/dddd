4c1de7e646d760430c35496d04de4dde78af7193
jun3993 <08320978600z@gmail.com>
2025-04-24 20:22:56 +0900

    Update wrapper.lua

--- wrapper.lua
+++ wrapper.lua
@@ -4,4 +4,4 @@ local autobondUrl = "https://raw.githubusercontent.com/thiennrb7/Script/main/aut
 -- 텔레포트 이벤트 바인딩local function bindTeleportListener()    local Players = game:GetService("Players")    local player = Players.LocalPlayer    if player then        player.OnTeleport:Connect(function(state)            if state == Enum.TeleportState.Started then                queueWrapper() -- 텔레포트 시작 시 다시 예약            end        end)    endend
 -- 초기 실행executeAutobond()
 -- 30초 후 추가 실행task.delay(30, function()    executeAutobond()end)
--- 텔레포트 예약 및 이벤트 연결queueWrapper()bindTeleportListener()
+-- 텔레포트. 예약 및 이벤트 연결queueWrapper()bindTeleportListener()
